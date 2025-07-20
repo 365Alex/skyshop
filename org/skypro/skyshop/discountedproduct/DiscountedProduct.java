fe91c6d17195ff0c92d98e68a78c12f77f842fee
master <aleshenko_ira_2017@mail.ru>
2025-07-20 20:44:40 +0300

    sky shop home work2

--- org/skypro/skyshop/discountedproduct/DiscountedProduct.java
+++ org/skypro/skyshop/discountedproduct/DiscountedProduct.java
@@ -3,24 +3,25 @@ package org.skypro.skyshop.discountedproduct;
 import org.skypro.skyshop.product.Product;
 
 public class DiscountedProduct extends Product {
-    private int basPrice;
-    private int discountPrice;
+    private int basePrice;
+    private int percent;
+    private  int discount;
 
-    public DiscountedProduct(String name, int basPrice, int discountPrice){
+    public DiscountedProduct(String name, int basPrice, int percent){
         super(name);
-        this.basPrice = basPrice;
-        this.discountPrice = discountPrice;
+        this.basePrice = basPrice;
+        this.percent = percent;
     }
 
     @Override
     public int getPrice(){
-        discountPrice = basPrice - (basPrice * discountPrice/100);
-        return discountPrice;
+        discount = basePrice - (basePrice * percent /100);
+        return discount;
     }
 
     @Override
     public String toString(){
-        return "Продукт: " + this.getName() +  " стоимость без скидки: " + this.basPrice + " руб." + " скидка: " + this.discountPrice + "%";
+        return "Продукт: " + this.getName() +  " стоимость без скидки: " + this.basePrice + " руб." + " скидка: " + this.percent + "%" + " цена со скидкой: " + this.getPrice() + " руб.";
     }
 
     @Override
