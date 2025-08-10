f3bb7e293890ce46088d860eca0bd1f36ab4f68b
master <aleshenko_ira_2017@mail.ru>
2025-08-10 20:46:54 +0300

    list

--- org/skypro/skyshop/App.java
+++ org/skypro/skyshop/App.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
 
+
         System.out.println("Корзина №1");
         ProductBasket productBasket = new ProductBasket(5);
         DiscountedProduct cheese = new DiscountedProduct("сыр", 85, 10);
